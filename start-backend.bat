1a78e1941b2ce9a106c40fb78310446086e5e42d
PavankumarKanakati <kanakatipavankumar01@gmail.com>
2025-04-16 14:36:42 +0530

    Update start-backend.bat

--- start-backend.bat
+++ start-backend.bat
@@ -2,7 +2,7 @@
 echo 🚀 Starting Health Checker Backend...
 
 :: Start the Spring Boot app using the JAR file
-start java -jar target\demo-0.0.1-SNAPSHOT.jar
+start "" java -XX:+TieredCompilation -XX:TieredStopAtLevel=1 -jar target\demo-0.0.1-SNAPSHOT.jar
 
 :: Wait 10 seconds for the backend to boot
 timeout /t 10 > nul
