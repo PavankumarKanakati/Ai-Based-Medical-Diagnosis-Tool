3f28ae62924e88b3f43536b2d72da36f58cc74c0
unknown <kanakatipavankumar01@gmail.com>
2025-04-17 20:46:32 +0530

    Added jre.zip.zip and local updates before syncing

--- src/main/java/com/example/medicaldiagnosis/MedicalDiagnosisApplication.java
+++ src/main/java/com/example/medicaldiagnosis/MedicalDiagnosisApplication.java
@@ -1,7 +1,5 @@
 package com.example.medicaldiagnosis;  // Change according to your package name
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
