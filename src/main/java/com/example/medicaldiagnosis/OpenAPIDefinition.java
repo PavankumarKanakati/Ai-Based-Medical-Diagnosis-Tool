c6bf727ef456a522209854bee8ed7656766065da
PavankumarKanakati <kanakatipavankumar01@gmail.com>
2025-04-29 16:24:25 +0530

    Update OpenAPIDefinition.java

--- src/main/java/com/example/medicaldiagnosis/OpenAPIDefinition.java
+++ src/main/java/com/example/medicaldiagnosis/OpenAPIDefinition.java
@@ -1,17 +1,6 @@
-<<<<<<< HEAD
+
 package com.example.medicaldiagnosis;
 
 public @interface OpenAPIDefinition {
-
     Info info();
-
 }
-=======
-package com.example.medicaldiagnosis;
-
-public @interface OpenAPIDefinition {
-
-    Info info();
-
-}
->>>>>>> 700ee257729cf11ddf30926d3fbc2c4bfa1e60a0
