6db0e3598121b61bfb04fcc6615c3d5fbfb62902
PavankumarKanakati <kanakatipavankumar01@gmail.com>
2025-04-29 16:23:38 +0530

    Update Info.java

--- src/main/java/com/example/medicaldiagnosis/Info.java
+++ src/main/java/com/example/medicaldiagnosis/Info.java
@@ -1,25 +1,7 @@
-<<<<<<< HEAD
 package com.example.medicaldiagnosis;
 
 public @interface Info {
-
     String title();
-
     String version();
-
     String description();
-
 }
-=======
-package com.example.medicaldiagnosis;
-
-public @interface Info {
-
-    String title();
-
-    String version();
-
-    String description();
-
-}
->>>>>>> 700ee257729cf11ddf30926d3fbc2c4bfa1e60a0
